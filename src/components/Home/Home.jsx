abfa8eb3bc1e83c6e9b6b78cf99e12d66abfa4f4
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-27 22:18:35 +0600

    add photo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import "./Home.css"
+import man from "../../assets/man.png"
 
 const Home = () => {
   return (
-   
-      <div id="home">
+
+    <div id="home">
+      <div className="lefthome">
 
       </div>
-    
+      <div className="righthome">
+        <img src={man} alt="" />
+      </div>
+    </div>
+
   )
 }
 

02b092ec20372058b08c6705a8727b4f5985ee2a
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-27 15:51:34 +0600

    for menu

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,13 +6,36 @@ const Nav = () => {
     <div>
       <nav>
         <h1>Hasib Hasan</h1>
-        <ul>
 
+        <ul className='PcMenu'>
+          <Link to="home" activeClass="active" spy={true} smooth={true} duration={500}>
+            <li>Home</li>
+          </Link>
+          <Link to="about" activeClass="active" spy={true} smooth={true} duration={500}>
+            <li>About</li>
+          </Link>
+          <Link to="project" activeClass="active" spy={true} smooth={true} duration={500}>
+            <li>Projects</li>
+          </Link>
+          <Link to="contact" activeClass="active" spy={true} smooth={true} duration={500}>
+            <li>Contact</li>
+          </Link>
 
-          <Link to="home" activeClass="active" spy={true} smooth={true} duration={500}> <li>Home</li></Link>
-          <Link to="about" activeClass="active" spy={true} smooth={true} duration={500} >  <li>About</li></Link>
-          <Link to="project" activeClass="active" spy={true} smooth={true} duration={500}><li>Projects</li> </Link>
-          <Link to="contact" activeClass="active" spy={true} smooth={true} duration={500}> <li>Contact</li></Link>
+          {/* =========================  mobileMenu   ========================================= */}
+        </ul>
+        <ul className='mobileMenu'>
+          <Link to="home" activeClass="active" spy={true} smooth={true} duration={500}>
+            <li>Home</li>
+          </Link>
+          <Link to="about" activeClass="active" spy={true} smooth={true} duration={500}>
+            <li>About</li>
+          </Link>
+          <Link to="project" activeClass="active" spy={true} smooth={true} duration={500}>
+            <li>Projects</li>
+          </Link>
+          <Link to="contact" activeClass="active" spy={true} smooth={true} duration={500}>
+            <li>Contact</li>
+          </Link>
 
 
         </ul>

fdd2b7f796e0adf000a0d629480740d61b69215b
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-27 15:39:45 +0600

    add active

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -9,10 +9,10 @@ const Nav = () => {
         <ul>
 
 
-          <Link> <li>Home</li></Link>
-          <Link>  <li>About</li></Link>
-          <Link><li>Projects</li> </Link>
-          <Link> <li>Contact</li></Link>
+          <Link to="home" activeClass="active" spy={true} smooth={true} duration={500}> <li>Home</li></Link>
+          <Link to="about" activeClass="active" spy={true} smooth={true} duration={500} >  <li>About</li></Link>
+          <Link to="project" activeClass="active" spy={true} smooth={true} duration={500}><li>Projects</li> </Link>
+          <Link to="contact" activeClass="active" spy={true} smooth={true} duration={500}> <li>Contact</li></Link>
 
 
         </ul>

357a8b5cc46030df4bffa8327b7af0a322ae89de
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-27 22:46:34 +0600

    Fixed Typing Effect

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
-import "./Home.css"
-import man from "../../assets/man.png"
+import React from 'react';
+import { Typewriter } from 'react-simple-typewriter';
+import man from "../../assets/man.png";
+import './Home.css';
 
 const Home = () => {
   return (
-
     <div id="home">
       <div className="lefthome">
-
+        <div className="homedetails">
+          <div className="line1">I'M</div>
+          <div className="line2">Hasib Hasan</div>
+          <div className="line3">
+            <Typewriter
+              words={['Web Developer', 'React Developer']}
+              loop
+              cursor
+              cursorStyle='_|'
+              typeSpeed={100}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </div>
+        </div>
       </div>
       <div className="righthome">
         <img src={man} alt="" />
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;

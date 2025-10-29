c261d6c989031bccba0f2480c851823bbe173989
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-29 15:25:51 +0600

    set up line , cirele

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,26 @@ import "./About.css"
 const About = () => {
     return (
         <div id="about">
+            <div className="leftabout">
+<div className="cirele-line">
+    {/* ================ 1st=================== */}
+    <div className="cirele"> </div>
+    <div className="line"></div>
+    {/* ================ 2nd   =================== */}
+    
+    <div className="cirele"> </div>
+    <div className="line"></div>
+   
+    {/* ================ 3rd =================== */}
+    <div className="cirele"> </div>
+    <div className="line"></div>
+    
+    
+</div>
+            </div>
+            <div className="rightabout">
 
+            </div>
         </div>
     )
 }

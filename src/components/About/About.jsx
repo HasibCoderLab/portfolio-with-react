7aa88a748a46420462884e9beff79d154e675cf8
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-29 15:35:25 +0600

    cerate ul, li

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,21 +4,31 @@ const About = () => {
     return (
         <div id="about">
             <div className="leftabout">
-<div className="cirele-line">
-    {/* ================ 1st=================== */}
-    <div className="cirele"> </div>
-    <div className="line"></div>
-    {/* ================ 2nd   =================== */}
-    
-    <div className="cirele"> </div>
-    <div className="line"></div>
-   
-    {/* ================ 3rd =================== */}
-    <div className="cirele"> </div>
-    <div className="line"></div>
-    
-    
-</div>
+                <div className="cirele-line">
+                    {/* ================ 1st=================== */}
+                    <div className="cirele"> </div>
+                    <div className="line"></div>
+                    {/* ================ 2nd   =================== */}
+
+                    <div className="cirele"> </div>
+                    <div className="line"></div>
+
+                    {/* ================ 3rd =================== */}
+                    <div className="cirele"> </div>
+                    <div className="line"></div>
+                </div>
+                {/* ========================== aboutdetails ========================= */}
+                <div className="aboutdetails">
+                    <div className="personalinfo">
+                        <h1>
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </h1>
+                    </div>
+                </div>
             </div>
             <div className="rightabout">
 
